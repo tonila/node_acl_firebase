a45f9177ddb76a5bddd41539bd2b46bc783b19df
Toni Lahnalampi <toni.lahnalampi@gmail.com>
2014-07-30 08:51:24 +0300

    first step to make independent firebase backend module.

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-module.exports = require("./lib/acl.js");
-module.exports.redisBackend = require("./lib/redis-backend.js");
-module.exports.memoryBackend = require("./lib/memory-backend.js");
-module.exports.mongodbBackend = require("./lib/mongodb-backend.js");
+module.exports = require("acl");
 module.exports.firebaseBackend = require("./lib/firebase-backend.js");
